34bec764d32988f7d12c3a9c7611f05c7bb7d6f0
s559222 <145594732+s559222@users.noreply.github.com>
2023-11-27 11:25:55 -0600

    Update Knight.java
    added methods for knight

--- src/project02startingfiles/Knight.java
+++ src/project02startingfiles/Knight.java
@@ -4,10 +4,21 @@
  */
 package project02startingfiles;
 
-/**
- *
- * @author S559222
- */
-public class Knight {
+class Knight extends Player {
 
+    @Override
+    public String getSpecialMove() {
+        return "Slash with Sword";
+    }
+
+    @Override
+    public void useSpecialMove() {
+        System.out.println("The Knight slashes with the sword!");
+    }
+
+    @Override
+    public String toString() {
+        return "Knight Status:\nHealth: " + getHealth() + "\nScore: " + getScore()
+                + "\nThe Knight's special move is '" + getSpecialMove() + "'";
+    }
 }
