534384113f90930e18d6d33d8b33fc4e96731e80
s559222 <145594732+s559222@users.noreply.github.com>
2023-11-27 11:16:23 -0600

    changes
    m

--- src/project02startingfiles/Player.java
+++ src/project02startingfiles/Player.java
@@ -8,6 +8,36 @@ package project02startingfiles;
  *
  * @author S559222
  */
-public class Player {
+public abstract class Player {
 
+    private int health;
+    private int score;
+
+    public Player() {
+        this.health = 10;
+        this.score = 0;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public abstract String getSpecialMove();
+
+    public abstract void useSpecialMove();
+
+    @Override
+    public abstract String toString();
 }
